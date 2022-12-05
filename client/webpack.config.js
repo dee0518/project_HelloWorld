8161c87b41d31c243d95748e6259c1c5d93e74d7
dee0518 <dehya-518@naver.com>
2022-12-05 15:58:19 +0900

    test 11

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
